f7a284cdb6439046ad4e4ab910be323b80225a25
samov004 <valerysamovich@gmail.com>
2014-12-14 01:26:19 -0500

    Removed white space

--- scripts/mail.php
+++ scripts/mail.php
@@ -9,21 +9,21 @@
 
 if(isset($_POST['submit'])){
 
-	// Variables declaration
-	$name = $_POST['name'];  // Gets the entered name
-	$email = $_POST['email']; // Gets the entered email
-	$message = $_POST['message']; // Gets the entered message
-	$to = "valerysamovich@gmail.com"; // Email address
+    // Variables declaration
+    $name = $_POST['name'];  // Gets the entered name
+    $email = $_POST['email']; // Gets the entered email
+    $message = $_POST['message']; // Gets the entered message
+    $to = "valerysamovich@gmail.com"; // Email address
 
-	// Validating the fields
-	if($name != "" && $email != "" && $message != ""){
-		// Mail function
-		mail($to, $name, $email, $message);
-		echo "<p style='margin-top:12px; text-align:center;'>Thank you for contact <span class='lightblue'>Express</span>&nbsp;<span class='boldblue'>Locksmith</span> team!</p>";
-		include ("includes/footer.php"); // Pull in the page footer.
-		exit(); // Quit the script.
-	}else{
-		echo "<p>Please fill in all fields and submit again!</p>";
-	}
+    // Validating the fields
+    if($name != "" && $email != "" && $message != ""){
+        // Mail function
+        mail($to, $name, $email, $message);
+        echo "<p style='margin-top:12px; text-align:center;'>Thank you for contact <span class='lightblue'>Express</span>&nbsp;<span class='boldblue'>Locksmith</span> team!</p>";
+        include ("includes/footer.php"); // Pull in the page footer.
+        exit(); // Quit the script.
+    }else{
+        echo "<p>Please fill in all fields and submit again!</p>";
+    }
 }
 ?>
